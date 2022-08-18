ae4dcaf0c35781356c576ab5303643d5a4e4ed1e
Tyler Madela <55926698+Developer-KT@users.noreply.github.com>
2022-08-18 21:38:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const highlightNavbar = () => {
     const navbar = document.querySelector(".nav")
+    const menuTabs = document.querySelector(".menu-tabs")
     let scrollPos = window.scrollY
 
     if (window.innerWidth > 960 && scrollPos > 0) {
         navbar.style.backgroundColor="white"
+        menuTabs.style.color="black"
+    } else {
+        navbar.style.backgroundColor="transparent"
+        menuTabs.style.color="white"
     }
+
+    const parallaxCon = document.querySelector(".parallax-container")
+    const swiper = document.querySelector(".swiper")
+
+    parallaxCon.style.top = scrollPos * 0.5 + 'px';
+    swiper.style.top = scrollPos * 0.7 + 'px';
 }
 
 window.addEventListener('scroll', highlightNavbar);
